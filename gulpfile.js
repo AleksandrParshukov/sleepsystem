afc59b6daa823f7eb0d5fd967d733f82ec4b0065
Aleksandr Parshukov <porshe97@yandex.ru>
2021-11-15 13:41:22 +0300

    начало

---
+++ gulpfile.js
@@ -0,0 +1,134 @@
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const sourcemap = require("gulp-sourcemaps");
+const less = require("gulp-less");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const csso = require("postcss-csso");
+const rename = require("gulp-rename");
+const htmlmin = require("gulp-htmlmin");
+const uglify = require("gulp-uglify");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
+const del = require("del");
+const sync = require("browser-sync").create();
+
+// Styles
+
+const styles = () => {
+  return gulp.src("source/less/style.less")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(less())
+    .pipe(gulp.dest("docs/css"))
+    .pipe(postcss([
+      autoprefixer(),
+      csso()
+    ]))
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("docs/css"))
+    .pipe(sync.stream());
+}
+
+exports.styles = styles;
+
+// JS
+
+const js = () => {
+  return gulp.src("source/js/*.js")
+    .pipe(sourcemap.init())
+    .pipe(uglify())
+    .pipe(rename({suffix: ".min"}))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("docs/js"))
+    .pipe(sync.stream());
+}
+
+exports.js = js;
+
+// HTML
+
+const html = () => {
+  return gulp.src("source/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true, conservativeCollapse: true }))
+    .pipe(gulp.dest("docs"));
+}
+
+// Copy
+
+const copy = (done) => {
+  gulp.src([
+    "source/fonts/*.{woff2,woff}",
+    "source/*.ico",
+    "source/img/**/*.{jpg,png,svg}",
+    "source/css/*.css",
+    "source/js/**/*.js",
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("docs"))
+  done();
+}
+
+exports.copy = copy;
+
+// Clean
+
+const clean = () => {
+  return del("docs");
+};
+
+// Server
+
+const server = (done) => {
+  sync.init({
+    server: {
+      baseDir: "docs"
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+}
+
+exports.server = server;
+
+// Reload
+
+const reload = done => {
+  sync.reload();
+  done();
+}
+
+// Watcher
+
+const watcher = () => {
+  gulp.watch("source/less/**/*.less", gulp.series(styles));
+  gulp.watch("source/js/*.js", gulp.series(js, reload));
+  gulp.watch("source/*.html", gulp.series(html, reload));
+}
+
+// docs
+
+const docs = gulp.series(
+  clean,
+  gulp.parallel(
+    styles,
+    js,
+    html,
+    copy
+  ));
+
+exports.docs = docs;
+
+// Default
+
+exports.default = gulp.series(
+  docs,
+  gulp.series(
+    server,
+    watcher
+  ));
---
+++ source/js/script.js
@@ -0,0 +1,119 @@
+/* ********** Swipers ********** */
+
+const LOCATIONS = ['"ГРАНД КАНЬОН"', '"МЕБЕЛЬНЫЙ КОНТИНЕНТ"', '"МЕБЕЛЬ СИТИ 2"'];
+
+const location_swiper = new Swiper ('.location__swiper', {
+  direction: 'horizontal',
+  loop: true,
+  effect: 'fade',
+  fadeEffect: {
+      crossFade: true
+    },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (LOCATIONS[index]) + '</span>';
+      },
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    992: {
+      direction: 'vertical',
+    },
+  }
+})
+
+const reviews_swiper = new Swiper ('.reviews__list', {
+  loop: false,
+  slidesPerView: "auto",
+  navigation: {
+    nextEl: '.reviews__control--next',
+    prevEl: '.reviews__control--prev',
+  },
+})
+
+const recommendations_swiper = new Swiper ('.recommendations__list', {
+  loop: true,
+  slidesPerView: "auto",
+  navigation: {
+    nextEl: '.recommendations__swiper_control--next',
+    prevEl: '.recommendations__swiper_control--prev',
+  },
+})
+
+$.each($('.contacts__swiper'), function() {
+  console.log($(this).attr('id'))
+  const contacts_swiper = new Swiper (`#${$(this).attr('id')}`, {
+    loop: true,
+    slidesPerView: "auto",
+    navigation: {
+      nextEl: `#${$(this).attr('id')} .contacts__swiper_next`,
+      prevEl: `#${$(this).attr('id')} .contacts__swiper_prev`,
+    },
+  })
+})
+
+
+
+
+
+/* ********** Filter ********** */
+const $filter__dropdown = $('.filter__dropdown');
+
+$.each($filter__dropdown, function() {
+  const $dropdown = $(this),
+        $title = $dropdown.find('.filter__title');
+
+  $title.on('click', function(evt) {
+    evt.preventDefault();
+
+    $dropdown.toggleClass('open');
+  })
+})
+
+
+
+/* ********** Footer Menu ********** */
+
+const $header__open_menu = $('.header__open_menu'),
+      $header__close_menu = $('.header__close_menu'),
+      $header__main_nav_container = $('.header__main_nav_container'),
+      $header__search_btn = $('.header__search_btn');
+
+$header__open_menu.on('click', function(evt) {
+  evt.preventDefault();
+
+  $header__main_nav_container.addClass('open');
+})
+
+$header__close_menu.on('click', function(evt) {
+  evt.preventDefault();
+
+  $header__main_nav_container.removeClass('open');
+})
+
+$header__search_btn.on('click', function(evt) {
+  evt.preventDefault();
+
+  $header__search_btn.parent().toggleClass('open');
+})
+
+
+
+
+
+/* ********** Footer Menu ********** */
+
+const $footer_menu = $('.footer_menu');
+
+$.each($footer_menu, function() {
+  const $menu = $(this);
+        $title = $menu.find('.footer_menu__title');
+
+  $title.on('click', function(evt) {
+    evt.preventDefault();
+
+    $menu.toggleClass('open');
+  })
+})
